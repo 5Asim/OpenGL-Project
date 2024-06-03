92832d904341c4ec92c1650b712bb5e7959ce1f2
5Asim <ashim.shrestha.55@gmail.com>
2024-06-03 07:29:29 +0545

    major changes

--- main.py
+++ main.py
@@ -1,92 +1,328 @@
+import os
+import glfw
 import pygame
-from pygame.locals import *
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from shapes import draw_cube, draw_sphere, draw_pyramid
+from maze import *
+from car import *
+from collision import *
+from Healthbar import *
+from texture import *
+from OpenGL.GLUT import GLUT_STROKE_MONO_ROMAN
 
-# Initialize Pygame and OpenGL
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 700
+PERIOD = 10
+First_Start_Flag = True
+Go_Drive_Flag = False
+Go_Back_Flag = False
+Break_Flag = False
+On_button = False
+Song_Flag = False
+mouse_x, mouse_y = 0, 0
+start_game = 0
+game_over = 0
+credits_sc = 0
+you_win = 0
+carModel = car()
+
 pygame.init()
-display = (800, 600)
-pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-glOrtho(-5, 5, -5, 5, -5, 5)
+font = pygame.font.Font(None, 36) 
+sounds = [pygame.mixer.Sound("Sound/crash.wav"),
+          pygame.mixer.Sound("Sound/coin.wav"),
+          pygame.mixer.Sound("Sound/revive.wav"),
+          pygame.mixer.Sound("Sound/car_horn.wav"),
+          pygame.mixer.Sound("Sound/starting_game.wav"),
+          pygame.mixer.Sound("Sound/go_driving.wav"),
+          pygame.mixer.Sound("Sound/car_reverse.wav"),
+          pygame.mixer.Sound("Sound/car_break.wav"),
+          pygame.mixer.Sound("Sound/song.wav"),
+          pygame.mixer.Sound("Sound/lobby_music.wav"),
+          pygame.mixer.Sound("Sound/mouse_point.wav"),
+          pygame.mixer.Sound("Sound/car_reverse1.wav"),
+          pygame.mixer.Sound("Sound/bomb.wav"),
+          pygame.mixer.Sound("Sound/bravo.wav")
+          ]
+sounds[8].set_volume(0.3)
+sounds[10].set_volume(0.5)
+sounds[11].set_volume(0.08)
 
-# Parameters for transformations
-initial_translate = [0, 0, 0]
-initial_rotate = [0, 0, 0]
-initial_scale = [1, 1, 1]
 
-translate = initial_translate.copy()
-rotate = initial_rotate.copy()
-scale = initial_scale.copy()
+def init_proj():
+    glClearColor(0, 0, 0, 0)
+    glMatrixMode(GL_PROJECTION)
+    glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, 0, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    load_texture()
 
-shape_index = 0
 
-shapes = [draw_cube, draw_sphere, draw_pyramid]
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    global start_game, game_over, you_win, mouse_x, mouse_y
+    start_game = 1
+    game_over
+    you_win
+    mouse_x
+    mouse_y
 
-def handle_input():
-    global translate, rotate, scale, shape_index
-    keys = pygame.key.get_pressed()
-    if keys[K_LEFT]:
-        translate[0] -= 0.1
-    if keys[K_RIGHT]:
-        translate[0] += 0.1
-    if keys[K_UP]:
-        translate[1] += 0.1
-    if keys[K_DOWN]:
-        translate[1] -= 0.1
-    if keys[K_q]:
-        translate[2] -= 0.1
-    if keys[K_e]:
-        translate[2] += 0.1
-    if keys[K_a]:
-        rotate[1] -= 5
-    if keys[K_d]:
-        rotate[1] += 5
-    if keys[K_w]:
-        rotate[0] -= 5
-    if keys[K_s]:
-        rotate[0] += 5
-    if keys[K_z]:
-        scale[0] *= 0.9
-        scale[1] *= 0.9
-        scale[2] *= 0.9
-    if keys[K_x]:
-        scale[0] *= 1.1
-        scale[1] *= 1.1
-        scale[2] *= 1.1
-    if keys[K_TAB]:
-        shape_index = (shape_index + 1) % len(shapes)
-    if keys[K_r]:
-        reset_transformations()
+    if carModel.health <= 0:
+        start_game = 2
+        game_over = 1
 
-def reset_transformations():
-    global translate, rotate, scale
-    translate = initial_translate.copy()
-    rotate = initial_rotate.copy()
-    scale = initial_scale.copy()
+    if credits_sc == 1:
+        if 260 <= mouse_x <= 460 and 600 <= mouse_y <= 680:
+            draw_texture(260, 20, 460, 100, BACK_RED)
+        else:
+            draw_texture(260, 20, 460, 100, BACK_YELLOW)
+        draw_texture(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, CREDIT_SCREEN)
 
-def main():
-    clock = pygame.time.Clock()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    elif start_game == 0:
+        glLoadIdentity()
+        if 280 <= mouse_x <= 520 and 280 <= mouse_y <= 360:
+            draw_texture(280, 340, 520, 420, START_RED)
+        else:
+            draw_texture(280, 340, 520, 420, START_YELLOW)
 
-        handle_input()
+        if 280 <= mouse_x <= 520 and 380 <= mouse_y <= 460:
+            draw_texture(280, 240, 520, 320, CREDIT_RED)
+        else:
+            draw_texture(280, 240, 520, 320, CREDIT_YELLOW)
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        if 280 <= mouse_x <= 520 and 480 <= mouse_y <= 560:
+            draw_texture(280, 140, 520, 220, EXIT_RED)
+        else:
+            draw_texture(280, 140, 520, 220, EXIT_YELLOW)
+        draw_texture(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, START_SCREEN)
+
+    elif start_game == 1:
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        cen = carModel.center()
+        glOrtho(cen[0] - 300, cen[0] + 300, cen[1] - 175, cen[1] + 175, -1, 1)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glClearColor(0.2, 0.2, 0.2, 0)
+
+        if test_car_walls(carModel, maze1):
+            carModel.collosion = True
+            sounds[0].set_volume(0.2)
+            sounds[0].play(0)
+            sounds[5].stop()
+            sounds[6].stop()
+
+        if test_car_bomb(carModel, bombs1):
+            sounds[12].set_volume(0.5)
+            sounds[12].play(0)
+            carModel.health -= 50
+
+        if test_car_coin(carModel, coins1):
+            carModel.coins += 1
+            sounds[1].play(0)
+
+        if test_car_health(carModel, health1):
+            carModel.health = carModel.health + 20 if carModel.health + 20 < 100 else 100
+            sounds[2].set_volume(0.2)
+            sounds[2].play(0)
+
+        if test_car_finish(carModel, finish):
+            sounds[13].set_volume(0.5)
+            sounds[13].play(0)
+            you_win = 1
+            start_game = 4
+
+        draw_health(carModel.health, cen)
         glPushMatrix()
-        glTranslatef(*translate)
-        glRotatef(rotate[0], 1, 0, 0)
-        glRotatef(rotate[1], 0, 1, 0)
-        glRotatef(rotate[2], 0, 0, 1)
-        glScalef(*scale)
-        shapes[shape_index]()
+        s = "stars : " + str(carModel.coins)
         glPopMatrix()
 
-        pygame.display.flip()
-        clock.tick(60)
+        draw_map()
+        draw_coins()
+        draw_healthkit()
+        draw_bombs()
+        draw_finish()
+
+        glPushMatrix()
+        carModel.animation()
+        carModel.draw()
+        glPopMatrix()
+
+    elif game_over == 1:
+        sounds[8].stop()
+        glClearColor(0, 0, 0, 0)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, 0, 1)
+        glMatrixMode(GL_MODELVIEW)
+
+        if 480 <= mouse_x <= 720 and 450 <= mouse_y <= 550:
+            draw_texture(480, 150, 720, 250, TRY_AGAIN_RED)
+        else:
+            draw_texture(480, 150, 720, 250, TRY_AGAIN_YEL)
+
+        if 480 <= mouse_x <= 720 and 570 <= mouse_y <= 670:
+            draw_texture(480, 30, 720, 130, EXIT2_RED)
+        else:
+            draw_texture(480, 30, 720, 130, EXIT2_YEL)
+
+        draw_texture(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, PLAY_AGAIN)
+
+    elif you_win == 1:
+        sounds[8].stop()
+        glClearColor(0, 0, 0, 0)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, 0, 1)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        if 480 <= mouse_x <= 720 and 570 <= mouse_y <= 670:
+            draw_texture(480, 30, 720, 130, HOME_RED)
+        else:
+            draw_texture(480, 30, 720, 130, HOME_YEL)
+        draw_texture(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, YOU_WIN)
+
+    glfw.swap_buffers(window)
+
+
+def draw_texture(left, bottom, right, top, tex_iden):
+    glBindTexture(GL_TEXTURE_2D, tex_iden)
+    glColor3f(1, 1, 1)
+    glBegin(GL_POLYGON)
+    glTexCoord2f(0, 0)
+    glVertex2f(left, bottom)
+    glTexCoord2f(1, 0)
+    glVertex2f(right, bottom)
+    glTexCoord2f(1, 1)
+    glVertex2f(right, top)
+    glTexCoord2f(0, 1)
+    glVertex2f(left, top)
+    glEnd()
+    glBindTexture(GL_TEXTURE_2D, -1)
+
+
+
+def handle_keys(window, key, scancode, action, mods):
+    global carModel, Go_Drive_Flag, Go_Back_Flag, Break_Flag, Song_Flag
+
+    if action == glfw.PRESS or action == glfw.REPEAT:
+        if key == glfw.KEY_W:
+            carModel.speed = 2.5
+            if not Go_Drive_Flag and start_game == 1:
+                sounds[5].set_volume(0.1)
+                sounds[5].play(-1)
+                Go_Drive_Flag = True
+
+        elif key == glfw.KEY_S:
+            carModel.speed = -2
+            if not Go_Back_Flag and start_game == 1:
+                sounds[6].set_volume(0.5)
+                sounds[6].play(-1)
+                Go_Back_Flag = True
+
+        elif key == glfw.KEY_A:
+            carModel.rot = 1
+
+        elif key == glfw.KEY_D:
+            carModel.rot = -1
+
+        elif key == glfw.KEY_SPACE:
+            carModel.speed = 0
+            if not Break_Flag and start_game == 1:
+                sounds[7].set_volume(0.2)
+                sounds[7].play(0)
+                Break_Flag = True
+
+    elif action == glfw.RELEASE:
+        if key == glfw.KEY_W:
+            carModel.speed = 0
+            sounds[5].stop()
+            Go_Drive_Flag = False
+
+        elif key == glfw.KEY_S:
+            carModel.speed = 0
+            sounds[6].stop()
+            Go_Back_Flag = False
+
+        elif key == glfw.KEY_A:
+            carModel.rot = 0  # Reset rotation when 'A' is released
+
+        elif key == glfw.KEY_D:
+            carModel.rot = 0  # Reset rotation when 'D' is released
+
+        elif key == glfw.KEY_SPACE:
+            Break_Flag = False
+
+
+
+
+def handle_mouse_button(window, button, action, mods):
+    global start_game, credits_sc, you_win, game_over
+
+    if action == glfw.PRESS and button == glfw.MOUSE_BUTTON_LEFT:
+        x, y = glfw.get_cursor_pos(window)
+        y = WINDOW_HEIGHT - y
+        global mouse_x, mouse_y
+        mouse_x, mouse_y = int(x), int(y)
+        sounds[10].play(0)
+
+        if credits_sc == 1:
+            if 260 <= mouse_x <= 460 and 600 <= mouse_y <= 680:
+                credits_sc = 0
+
+        elif start_game == 0:
+            if 280 <= mouse_x <= 520 and 380 <= mouse_y <= 460:
+                credits_sc = 1
+            if 280 <= mouse_x <= 520 and 280 <= mouse_y <= 360:
+                sounds[4].set_volume(0.2)
+                sounds[4].play(0)
+                start_game = 1
+            if 280 <= mouse_x <= 520 and 480 <= mouse_y <= 560:
+                exit(0)
+
+        elif game_over == 1:
+            if 480 <= mouse_x <= 720 and 450 <= mouse_y <= 550:
+                carModel.__init__()
+                game_over = 0
+                start_game = 0
+            if 480 <= mouse_x <= 720 and 570 <= mouse_y <= 670:
+                exit(0)
+
+        elif you_win == 1:
+            if 480 <= mouse_x <= 720 and 570 <= mouse_y <= 670:
+                you_win = 0
+                carModel.__init__()
+                start_game = 0
+
+        else:
+            if 280 <= mouse_x <= 520 and 280 <= mouse_y <= 360:
+                start_game = 1
+
+
+def main():
+    if not glfw.init():
+        return
+
+    global window
+    window = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Car Game", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    glfw.set_key_callback(window, handle_keys)
+    glfw.set_mouse_button_callback(window, handle_mouse_button)
+    glfw.swap_interval(1)
+    init_proj()
+
+    while not glfw.window_should_close(window):
+        display()
+        glfw.poll_events()
+
+    glfw.terminate()
+
 
 if __name__ == "__main__":
     main()
